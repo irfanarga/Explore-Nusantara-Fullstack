d67aca270d17160e8628f2e4187a4f20a160bfdb
irfanarga <irfanargaprasetya81@gmail.com>
2024-12-04 22:09:14 +0700

    Adjust home page

--- public/js/validate-form.js
+++ public/js/validate-form.js
@@ -16,3 +16,10 @@
     }, false)
   })
 })()
+
+const menuButton = document.getElementById('menuButton');
+const menu = document.getElementById('menu');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
