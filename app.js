312030dec147bc363e02bda18294e2fd7535a940
irfanarga <irfanargaprasetya81@gmail.com>
2024-12-04 08:52:15 +0700

    Fixing cors and change home page

--- app.js
+++ app.js
@@ -29,11 +29,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // middlewares
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
