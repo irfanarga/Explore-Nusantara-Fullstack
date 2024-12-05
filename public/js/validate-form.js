7964d663271dca220a2fbcb13c3c81ea9043f97a
irfanarga <irfanargaprasetya81@gmail.com>
2024-12-05 14:21:17 +0700

    Add responsive show

--- public/js/validate-form.js
+++ public/js/validate-form.js
@@ -23,4 +23,5 @@ const menu = document.getElementById('menu');
 menuButton.addEventListener('click', () => {
   menu.classList.toggle('-translate-x-full');
   menu.classList.toggle('translate-x-0');
+  menu.classList.toggle('hidden');
 });
