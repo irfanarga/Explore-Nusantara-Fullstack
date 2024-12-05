d8ff7d67995ac4e69a9c71425d21c1f0c51fc24a
irfanarga <irfanargaprasetya81@gmail.com>
2024-12-05 10:00:20 +0700

    Change port

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 })
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(80, () => {
+  console.log('Server is running on port 80');
 })
